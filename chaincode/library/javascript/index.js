4f71116b221add8210b320f85ea397d124cba2d7
jeonsanggi <jsg2861@gmail.com>
2019-05-08 10:04:14 +0900

    first commit

---
+++ chaincode/library/javascript/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const library = require('./lib/library');
+
+module.exports.library = library;
+module.exports.contracts = [ library ];
---
+++ chaincode/library/typescript/src/index.ts
@@ -0,0 +1,8 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { library } from './library';
+export { library } from './library';
+
+export const contracts: any[] = [ library ];
